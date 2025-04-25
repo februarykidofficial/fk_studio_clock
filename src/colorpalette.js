9a44196fa41a971b69e4bb5b0134f448a118c3e9
februarykidofficial <februarykidofficial@gmail.com>
2025-04-25 12:47:39 +0200

    Added Color Palette

--- src/colorpalette.js
+++ src/colorpalette.js
@@ -1,33 +1,3 @@
-function displayTime() {
-  let today = new Date();
-  let hours = today.getHours().toString().padStart(2, "0");
-  let minutes = today.getMinutes().toString().padStart(2, "0");
-  let seconds = today.getSeconds().toString().padStart(2, "0");
-  let clockElement = document.getElementById("clock");
-  clockElement.innerText = hours + ":" + minutes + ":" + seconds;
-}
-
-function displayDate() {
-  let today = new Date();
-  let date = today.toDateString();
-  let dateElement = document.getElementById("date");
-  dateElement.innerText = date;
-}
-
-setInterval(displayTime, 1000);
-setInterval(displayDate, 1000);
-
-displayDate();
-displayTime();
-
-
-
-
-
-
-
-
-
 const paletteToggle = document.querySelector('.palette-toggle');
 const palette = document.querySelector('.palette');
 
